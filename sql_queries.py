131bb0e5c5c53f375ce19f102c848b4ba2ffe148
fred <f.tischbein@gmail.com>
2019-11-24 20:53:01 +0100

    Finished INSERT queries, Added ON CONFLICTS

--- sql_queries.py
+++ sql_queries.py
@@ -45,13 +45,13 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (
 """)
 
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
-                            start_time int, 
+                            start_time timestamp PRIMARY KEY, 
                             hour int, 
                             day int, 
                             week int, 
                             month int, 
                             year int, 
-                            weekday varchar)
+                            weekday int)
 """)
 
 # INSERT RECORDS
@@ -59,20 +59,28 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
 songplay_table_insert = ("""
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) 
+                                VALUES (%s, %s, %s, %s, %s)
+                                ON CONFLICT (user_id) DO UPDATE set level = EXCLUDED.level
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration) 
                                 VALUES (%s, %s, %s, %s, %s)
+                                ON CONFLICT (song_id) DO NOTHING
 """)
 
 artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, latitude, longitude) 
                                 VALUES (%s, %s, %s, %s, %s)
+                                ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
+                                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                                ON CONFLICT (start_time) DO NOTHING
 """)
+
+# 'ts', 'hour', 'day', 'week', 'month', 'year', 'weekday'
 
 # FIND SONGS
 
